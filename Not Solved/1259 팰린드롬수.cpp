de5547145b06eafe98506c0ed9fc09f05101d2db
정윤혁 <yunhyukjeong02@gmail.com>
2020-07-03 21:53:17 +0900

    1259 Not Solved

--- Not Solved/1259 팰린드롬수.cpp
+++ Not Solved/1259 팰린드롬수.cpp
@@ -2,7 +2,25 @@
 //* Bronze 1
 
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
+    bool palindrome = false;
+    char number[100001];
+
+    while (true)
+    {
+        scanf("%s", number);
+        if (!strcmp(number, "0"))
+            return 0;
+        int len = strlen(number);
+        char number2[100001];
+        for (int i = 0; i < len; i++)
+            number2[i] = number[len - 1 - i];
+        if (!strcmp(number, number2))
+            printf("yes\n");
+        else
+            printf("no\n");
+    }
 }
